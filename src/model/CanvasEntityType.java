14852434f345ca66c69426c3bce44e236c5a97eb
DVasek <vasek@seit.cz>
2019-12-14 16:32:15 +0100

    canvas entity type

---
+++ src/model/CanvasEntityType.java
@@ -0,0 +1,29 @@
+package model;
+
+public enum CanvasEntityType {
+    LAKE("Lake"),
+    TREE("Tree"),
+    STREET("Street"),
+    BUSSTOP("Bus"),
+    BUILDING("Building"),
+    OTHER("Other");
+
+    private String label;
+
+    CanvasEntityType(String label) {
+        this.label = label;
+    }
+
+    public String toString() {
+        return label;
+    }
+
+    public static CanvasEntityType getByLabel(String code){
+        for(CanvasEntityType e : CanvasEntityType.values()){
+            if(code.equals(e.label)) {
+                return e;
+            }
+        }
+        return null;
+    }
+}
